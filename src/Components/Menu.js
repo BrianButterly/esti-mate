c29a0aa456584d276eb8750a44f72c0acd93a281
Brian Butterly <butterlybrian@gmail.com>
2021-09-13 19:21:41 -0500

    logo

--- src/Components/Menu.js
+++ src/Components/Menu.js
@@ -11,35 +11,40 @@ export default class MenuNav extends Component {
     const { activeItem } = this.state;
 
     return (
-      <Menu>
-        <Link to="/">
-          <Menu.Item
-            name="home"
-            active={activeItem === "home"}
-            onClick={this.handleItemClick}
-          >
-            Home
+      <div className="menu">
+            <img className="logo" src="./logo2.png" alt="logo" />
+        <Menu>
+          <Menu.Item>
           </Menu.Item>
-        </Link>
-        <Link to="/estimates">
-          <Menu.Item
-            name="estimates"
-            active={activeItem === "estimates"}
-            onClick={this.handleItemClick}
-          >
-            Estimates
-          </Menu.Item>
-        </Link>
-        <Link to="/create">
-          <Menu.Item
-            name="create new estimate"
-            active={activeItem === "create new estimate"}
-            onClick={this.handleItemClick}
-          >
-            Create New Estimate
-          </Menu.Item>
-        </Link>
-      </Menu>
+          <Link to="/">
+            <Menu.Item
+              name="home"
+              active={activeItem === "home"}
+              onClick={this.handleItemClick}
+            >
+              Home
+            </Menu.Item>
+          </Link>
+          <Link to="/estimates">
+            <Menu.Item
+              name="estimates"
+              active={activeItem === "estimates"}
+              onClick={this.handleItemClick}
+            >
+              Estimates
+            </Menu.Item>
+          </Link>
+          <Link to="/create">
+            <Menu.Item
+              name="create new estimate"
+              active={activeItem === "create new estimate"}
+              onClick={this.handleItemClick}
+            >
+              Create New Estimate
+            </Menu.Item>
+          </Link>
+        </Menu>
+      </div>
     );
   }
 }
